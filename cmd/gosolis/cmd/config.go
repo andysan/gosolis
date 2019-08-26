7e07db0abdcbfa387a20658c05b3b7720374ad34
Andreas Sandberg <andreas@sandberg.pp.se>
2019-08-26 16:57:55 +0100

    tool: Add command to save config

---
+++ cmd/gosolis/cmd/config.go
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2019 Andreas Sandberg
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+func configSaveMain(cmd *cobra.Command, args []string) {
+	viper.WriteConfigAs(args[0])
+}
+
+var configCmd = &cobra.Command{
+	Use:   "config",
+	Short: "Configuration",
+}
+
+var configSaveCmd = &cobra.Command{
+	Use:   "save FILE",
+	Short: "Save configuration to file",
+	Args:  cobra.ExactArgs(1),
+	Run:   configSaveMain,
+}
+
+func init() {
+	RootCmd.AddCommand(configCmd)
+	configCmd.AddCommand(configSaveCmd)
+}
